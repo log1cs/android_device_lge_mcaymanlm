19ba0610ddc4d9c141b016c28444c5333b8d1d2d
bengris32 <bengris32@protonmail.ch>
2025-12-20 15:00:11 +0000

    mcaymanlm: Configure media stack
    * Reference: https://github.com/moto-common/android_device_mediatek_common/blob/master/vendor/media-c2/proprietary-files.txt
    
    Change-Id: I63d5365f4c52ef8f366c66f45cc6f4210c705a8c
    Co-authored-by: electimon <electimon@gmail.com>
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -42,6 +42,13 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
+# Media
+PRODUCT_PACKAGES += \
+    libstagefright_softomx_plugin.vendor
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
