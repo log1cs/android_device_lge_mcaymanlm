3b15b25178dc6a704b393127a1a167a6b7cd7fd2
bengris32 <bengris32@protonmail.ch>
2025-12-20 15:40:06 +0000

    mcaymanlm: Enable support for EXT4 & F2FS formatted userdata
    Change-Id: I88ce22a250e1f7e5882cb225cca8691af587985a
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -101,6 +101,8 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # Recovery
 BOARD_USES_RECOVERY_AS_BOOT := true
 TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
 # SEPolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
