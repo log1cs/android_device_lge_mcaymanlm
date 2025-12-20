7395958de9b33a87ea2a6b5617c645e37b26fc26
Chris Fries <cfries@google.com>
2025-12-20 14:15:52 +0000

    mcaymanlm: Set printk.devkmsg=on on kernel cmdline to avoid ratelimit
    init and friends are getting ratelimted on /dev/kmsg.  Let's keep
    the logs by setting printk.devkmsg=on.
    
    Bug: 62952768
    Test: Check for init logs in kernel during boot
    Change-Id: I103e3cc1d73880af1d9a0cdc4dae05223eb4c427

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,7 @@ TARGET_NO_BOOTLOADER := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x40078000
-BOARD_KERNEL_CMDLINE := androidboot.init_fatal_reboot_target=recovery bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := androidboot.init_fatal_reboot_target=recovery bootopt=64S3,32N2,64N2 androidboot.selinux=permissive printk.devkmsg=on
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_KERNEL_PAGESIZE := 2048
