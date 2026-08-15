cf7312e82dc5629bfaabb94ac5b3b0ea748d4535
jmpfbmx <jmpf.bmx@gmail.com>
2026-08-15 06:15:06 +0000

    mcaymanlm: Add arch, bootloader and platform flags
    Change-Id: Ie0ca1d06a57a715867ceb4954cc37248cd099081
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,8 +5,30 @@
 
 DEVICE_PATH := device/lge/mcaymanlm
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a77
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := mcaymanlm
+TARGET_NO_BOOTLOADER := true
+
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_mcaymanlm_defconfig
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6885
 
 # Inherit the proprietary files
 include vendor/lge/mcaymanlm/BoardConfigVendor.mk
