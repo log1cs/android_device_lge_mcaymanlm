1fd47622a59dafa317d648b88ce4be1b7d539d6a
Mashopy <eliasgheeraert@gmail.com>
2026-08-25 15:36:52 +0000

    mcaymanlm: Inherit MSSI common overlays
    Change-Id: Iabe2f6efa7455400e0c4b0dd30dd6de57f2db1d0

--- device.mk
+++ device.mk
@@ -108,6 +108,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Overlay
+$(call inherit-product, hardware/mediatek/overlay/mssi.mk)
+
 # Properties
 include hardware/mediatek/configs/properties/vendor_logtag.mk
 
