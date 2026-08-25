a55db950f72e6abda2d80c6552104cc593a9a80e
Lokesh Gidra <lokeshgidra@google.com>
2026-08-25 14:46:02 +0000

    mcaymanlm: Use 6GB dalvik-heap config
    Bug: 145823510
    Test: MPTS
    Change-Id: I086d4a5c09634c3c7bd51ec5d08dcaa1551511b4

--- device.mk
+++ device.mk
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # A/B
 PRODUCT_PACKAGES += \
     update_engine \
