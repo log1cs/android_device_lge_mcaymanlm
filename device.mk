753d5b985281668fb1d20ed18ff3b9fe893075e6
Tuan Anh <tuan73176@gmail.com>
2026-09-14 15:05:47 +0000

    mcaymanlm: Configure graphics stack
    Change-Id: I0f80922e9920349c2c50ffc61dbc4e15b3c53034

--- device.mk
+++ device.mk
@@ -105,6 +105,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
 # Graphics
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.3-service
+
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack-service.mediatek
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
