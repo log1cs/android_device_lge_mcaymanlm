6950cf98dc417916968b89f0f7d358fa0386e597
Tuan Anh <tuan73176@gmail.com>
2026-09-14 15:14:21 +0000

    mcaymanlm: Build FMRadio
    Change-Id: I855d4da988e3106386ac350f815a128b06714b36

--- device.mk
+++ device.mk
@@ -100,6 +100,10 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# FM Radio
+PRODUCT_PACKAGES += \
+    FMRadio
+
 # GPS
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
